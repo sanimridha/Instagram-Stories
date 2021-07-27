3ab1636a19ef59db1e0c1e547ccd6bb8593be203
sanimridha <sanimridha307@gmail.com>
2021-07-27 13:04:14 +0600

    four story added

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Story from 'react-native-story'
 
 export default function App() {
+  const stories = [
+    {
+      id: "1",
+      source: require("./assets/story.jpg"),
+      user: "Ugur Erdal",
+      avatar: require("./assets/avatar.jpg")
+    },
+    {
+      id: "2",
+      source: require("./assets/story2.jpg"),
+      user: "Mustafa",
+      avatar: require("./assets/avatar.jpg")
+    },
+    {
+      id: "3",
+      source: require("./assets/story.jpg"),
+      user: "Ugur Erdal",
+      avatar: require("./assets/avatar.jpg")
+    },
+    {
+      id: "4",
+      source: require("./assets/story2.jpg"),
+      user: "Mustafa",
+      avatar: require("./assets/avatar.jpg")
+    },
+  ];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Story
+            unPressedBorderColor="#e95950"
+            pressedBorderColor="#ebebeb"
+            stories={stories}
+            footerComponent={
+                <TextInput
+                    placeholder="Send message"
+                    placeholderTextColor="white"
+                    style={styles.footerInput}
+                />
+            }
+        />
+
   );
 }
 
